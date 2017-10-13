66aa94998a792a041ae9eed371ccaa7f566f0093
antonio <antoniobelmardacosta@gmail.com>
2017-10-14 00:53:52 +0100

    this commit adds the student count as well as comments to the directory file

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first we print the list of students
 puts "The students of Villains Academy"
 puts "--------------------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#we then add a sentence which provides a count of the total number of students
+print "Overall we have "
+#we are using print as this method doesn't add new line characters allowing the sentence to stay in one line
+print 11
+puts " great students"
