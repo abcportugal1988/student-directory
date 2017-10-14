6941e1af0a9e706ed0965f68737c153ef1a1e7bd
antonio <antoniobelmardacosta@gmail.com>
2017-10-14 18:36:18 +0100

    this commit adds new methods to print the student list with name restrictions (starting letter and length)

--- directory.rb
+++ directory.rb
@@ -5,20 +5,42 @@ def print_header
 end
 
 #this method prints the students names and cohorts
-def print(students,letter)
+def print_list(students)
   students.each_with_index do |student,index|
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+#this method prints the students names and cohorts according to some restrictions
+def print_list_letter(students)
+  puts "You want to see students' names starting with which letter?"
+  letter=gets.chomp
+  students.each do |student|
     #only print students whose first letter of the name is the same as specified letter
     if student[:name][0].downcase==letter
-      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+#this method prints the students names and cohorts according to some restrictions
+def print_list_size(students)
+  size=12
+  students.each do |student|
+    #only print students whose total name (including spaces) is shorter than 12 characters
+    if student[:name].length<=size
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
 end
 
 #this method prints the footer of the student list which includes the current student count.
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students but we are only printing the ones starting with your requested letter."
+  print "Overall, we have #{students.count} great students but we are only printing "
+  puts "the ones according to your specifications."
 end
 
+#this method allow for the creation of the student list database by asking the user for input and storing it in an array and hashes
 def input_students
   puts "Please enter the name of the students."
   puts "To finish, just press \"return\" twice (or once if you don't want to add any students ;))"
@@ -40,6 +62,8 @@ def input_students
 end
 
 students = input_students
+
 print_header
-print(students,"a")
+print_list(students)
+print_list_size(students)
 print_footer(students)
