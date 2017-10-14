d7546de5ad4b68098df9f52891727a04fb90e015
antonio <antoniobelmardacosta@gmail.com>
2017-10-14 04:03:36 +0100

    this commit adds indices to the student list

--- directory.rb
+++ directory.rb
@@ -6,8 +6,8 @@ end
 
 #this method prints the students names and cohorts
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student,index|
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
