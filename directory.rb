1733b38463a8fac80033f9951db521899ced5175
antonio <antoniobelmardacosta@gmail.com>
2017-10-14 01:40:15 +0100

    this commit adds the cohort to the array of students as a symbol

--- directory.rb
+++ directory.rb
@@ -1,15 +1,16 @@
 #adding all students to an array
-students=["Dr. Hannibal Lecter",
-          "Darth Vader",
-          "Nurse Ratched",
-          "Michael Corleone",
-          "Alex DeLarge",
-          "The Wicked Witch of the West",
-          "Terminator",
-          "Freddy Krueger",
-          "The Joker",
-          "Joffrey Baratheon",
-          "Norman Bates"]
+students=[["Dr. Hannibal Lecter", :november],
+          ["Darth Vader", :november],
+          ["Nurse Ratched", :november],
+          ["Michael Corleone", :november],
+          ["Alex DeLarge", :november],
+          ["The Wicked Witch of the West", :november],
+          ["Terminator", :november],
+          ["Freddy Krueger", :november],
+          ["The Joker", :november],
+          ["Joffrey Baratheon", :november],
+          ["Norman Bates", :november]
+          ]
 
 #this method prints the heading of the student list
 def print_header
@@ -17,16 +18,16 @@ def print_header
   puts "--------------------------"
 end
 
-#this method prints a list from an array, it can be used for our students array or any other
-def print(names)
-  names.each do |name|
-    puts name
+#this method prints the students names and cohorts
+def print(students)
+  students.each do |student|
+    puts "#{student[0]} (#{student[1]} cohort)"
   end
 end
 
 #this method prints the footer of the student list which includes the current student count.
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students."
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students."
 end
 
 print_header
