d724431d84d96b2e762127f3a5db501b1e083ea4
antonio <antoniobelmardacosta@gmail.com>
2017-10-14 01:20:30 +0100

    this commit adds methods to print the header, body and footer of the student list

--- directory.rb
+++ directory.rb
@@ -1,13 +1,34 @@
 #adding all students to an array
-students=["Dr. Hannibal Lecter","Darth Vader","Nurse Ratched","Michael Corleone",
-  "Alex DeLarge","The Wicked Witch of the West", "Terminator", "Freddy Krueger", "The Joker",
-  "Joffrey Baratheon","Norman Bates"]
-#and now we print them
-puts "The students of Villains Academy"
-puts "--------------------------"
-#we use the each method to print each student from the students array
-students.each do |student|
-  puts student
+students=["Dr. Hannibal Lecter",
+          "Darth Vader",
+          "Nurse Ratched",
+          "Michael Corleone",
+          "Alex DeLarge",
+          "The Wicked Witch of the West",
+          "Terminator",
+          "Freddy Krueger",
+          "The Joker",
+          "Joffrey Baratheon",
+          "Norman Bates"]
+
+#this method prints the heading of the student list
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------------------"
 end
-#we then add a sentence which provides a count - using the .count method - of the total number of students
-puts "Overall, we have #{students.count} great students."
+
+#this method prints a list from an array, it can be used for our students array or any other
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+#this method prints the footer of the student list which includes the current student count.
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students."
+end
+
+print_header
+print(students)
+print_footer(students)
