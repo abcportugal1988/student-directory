c7849085471c5d75b7ef16b484764c0f38c8532d
antonio <antoniobelmardacosta@gmail.com>
2017-10-14 01:12:41 +0100

    this commit adds the each method to print the list of students

--- directory.rb
+++ directory.rb
@@ -5,16 +5,9 @@ students=["Dr. Hannibal Lecter","Darth Vader","Nurse Ratched","Michael Corleone"
 #and now we print them
 puts "The students of Villains Academy"
 puts "--------------------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-#we then add a sentence which provides a count of the total number of students
-puts "Overall we have #{students.count} great students"
+#we use the each method to print each student from the students array
+students.each do |student|
+  puts student
+end
+#we then add a sentence which provides a count - using the .count method - of the total number of students
+puts "Overall, we have #{students.count} great students."
